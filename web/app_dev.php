799a44da74f13f96a19238cc90c8c2e2da7c4be6
CAILBAUT Olivier <cailbaut.olivier@live.fr>
2012-02-14 22:43:26 +0100

    Ajout en début de script d'un fonction permettant de surcharger les magic quotes du php.ini
    Ceci car mon hébergeur refuse de faire quoi que ce soit à ce sujet.
    
    Même avec l'option désactivée, ceci peut rester en place sans problèmes.

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,4 +1,26 @@
 <?php
+//Fonction d'override des magic quotes
+// !!!! DOIT ABSOLUMENT RESTER ICI EN TOUT DEBUT DE SCRIPT !!!!!!
+//
+function stripslashes_r($var) // Fonction qui supprime l'effet des magic quotes
+{
+        if(is_array($var)) // Si la variable passée en argument est un array, on appelle la fonction stripslashes_r dessus
+        {
+                return array_map('stripslashes_r', $var);
+        }
+        else // Sinon, un simple stripslashes suffit
+        {
+                return stripslashes($var);
+        }
+}
+ 
+if(get_magic_quotes_gpc()) // Si les magic quotes sont activés, on les désactive avec notre super fonction ! ;)
+{
+   $_GET = stripslashes_r($_GET);
+   $_POST = stripslashes_r($_POST);
+   $_COOKIE = stripslashes_r($_COOKIE);
+}
+//Fin d'override des magic quotes
 
 // if you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
